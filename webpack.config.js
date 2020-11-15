4f0b72d32e8701a0b5ad506753dce386a65166d6
KMConner <KMConner@users.noreply.github.com>
2020-11-16 00:14:00 +0900

    Remove Japanese !

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,11 @@
-// pathモジュールを読み(output.pathに絶対パスを指定するため)
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-    // モードの設定。指定可能な値は、none, development ,production（デフォルト）
-    // https://webpack.js.org/concepts/mode/#mode-development
     mode: 'development',
-    // アプリケーションが実行を開始されるポイント(エントリーポイント)
-    // 配列で指定すると、すべての項目が実行される
-    // https://webpack.js.org/configuration/entry-context/#entry
     entry: './src/content_sctipts.ts',
     output: {
         filename: 'bundle.js',
-        // ビルド後のファイルが出力される"絶対パス"ディレクトリ
-        // https://webpack.js.org/configuration/output/#outputpath
         path: path.join(__dirname, 'dist/')
     },
     module: {
