7dae8ccea6a27f834796e5e7dfa3261788f160ff
KMConner <KMConner@users.noreply.github.com>
2020-11-15 23:49:47 +0900

    Redirect on navigation

--- src/content_sctipts.ts
+++ src/content_sctipts.ts
@@ -3,6 +3,7 @@ import * as xpath from 'xpath'
 import { DOMParser } from "xmldom"
 import * as parse5 from "parse5"
 import * as xmlser from "xmlserializer"
+import { browser } from "webextension-polyfill-ts"
 
 
 async function main(e: chrome.webNavigation.WebNavigationParentedCallbackDetails) {
@@ -14,6 +15,7 @@ async function main(e: chrome.webNavigation.WebNavigationParentedCallbackDetails
     var select = xpath.useNamespaces({ "xhtml": "http://www.w3.org/1999/xhtml" });
     const attr: any = select("/xhtml:html/xhtml:body/xhtml:main/xhtml:div/xhtml:div[2]/xhtml:div/xhtml:div[2]/xhtml:div[2]/xhtml:div[2]/xhtml:p[1]/xhtml:small[1]/xhtml:a/@href", doc)[0];
     console.log(attr.nodeValue)
+    await browser.tabs.update(e.tabId, { url: attr.nodeValue })
 }
 
-chrome.webNavigation.onBeforeNavigate.addListener(main, { url: [{ urlMatches: "https://stackoverrun.com/*" }] });
+chrome.webNavigation.onBeforeNavigate.addListener(main, { url: [{ urlMatches: "https://stackoverrun.com/ja/q/*" }] });
