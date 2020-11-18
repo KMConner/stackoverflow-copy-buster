8e35b6552985c7b76d13a5f59521c7ef4f9d86b8
KMConner <KMConner@users.noreply.github.com>
2020-11-19 01:36:19 +0900

    Implement popup view

--- src/popup.ts
+++ src/popup.ts
@@ -1,1 +1,37 @@
-// alert("Hellow world!!!!!")
+import * as siteList from './siteList'
+
+function createSiteNameElement(siteName: string): HTMLDivElement {
+    const elem = document.createElement('div')
+    elem.innerText = siteName
+    elem.className = 'site_dom'
+    return elem
+}
+
+function createSiteSwitchElement(siteName: string): HTMLDivElement {
+    const input = document.createElement('input')
+    input.type = 'checkbox'
+
+    const span = document.createElement('span')
+    span.className = 'slider round'
+
+    const label = document.createElement('label')
+    label.className = 'switch'
+    label.appendChild(input)
+    label.appendChild(span)
+
+    const elem = document.createElement('div')
+    elem.className = 'site_toggle'
+    elem.appendChild(label)
+
+    return elem
+}
+
+function main() {
+    const table = <HTMLDivElement>document.getElementById("site_table")
+    for (const site of siteList.copySites) {
+        table.appendChild(createSiteNameElement(site.name))
+        table.appendChild(createSiteSwitchElement(site.name))
+    }
+}
+
+window.addEventListener('load', main)
